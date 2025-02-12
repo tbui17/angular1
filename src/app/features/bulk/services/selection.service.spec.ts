473918c9147b9680ddb6afe259dba21808b4b155
* <tbui17@users.noreply.github.com>
2025-02-11 17:57:55 -0700

    add unit tests; refactor

---
+++ src/app/features/bulk/services/selection.service.spec.ts
@@ -0,0 +1,73 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SelectionService } from './selection.service';
+import { Injector, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+
+describe('SelectionService', () => {
+  let service: SelectionService;
+  let injector: Injector;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideExperimentalZonelessChangeDetection()],
+    });
+    service = TestBed.inject(SelectionService);
+    injector = TestBed.inject(Injector);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should select', () => {
+    service.select({
+      index: 0,
+      event: {},
+    });
+    expect(service.selected()).toEqual([0]);
+  });
+
+  it('should select range', () => {
+    service.select({
+      index: 2,
+      event: {},
+    });
+
+    service.select({
+      index: 4,
+      event: { shiftKey: true },
+    });
+
+    expect(service.selected()).toEqual([2, 3, 4]);
+  });
+
+  it('should clear', () => {
+    service.select({
+      index: 2,
+      event: {},
+    });
+
+    service.select({
+      index: 4,
+      event: { shiftKey: true },
+    });
+
+    expect(service.selected()).toEqual([2, 3, 4]);
+
+    service.clear();
+
+    expect(service.selected()).toEqual([]);
+  });
+
+  it('should trigger rerender on select', (done) => {
+    toObservable(service.selected, { injector }).subscribe(([x]) => {
+      expect(x === 10).toBeTrue();
+      done();
+    });
+    service.select({
+      index: 10,
+      event: {},
+    });
+  });
+});
---
+++ src/app/features/bulk/services/selection.service.ts
@@ -0,0 +1,81 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { produce, enableMapSet } from 'immer';
+
+enableMapSet();
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SelectionService {
+  private readonly _selected = signal(new Set<number>());
+  private readonly lastSelected = signal<number | null>(null);
+  readonly selected = computed(this.getSelectedValues.bind(this));
+
+  private getSelectedValues(): number[] {
+    return [...this._selected().values()];
+  }
+
+  select({
+    index,
+    event: { ctrlKey, shiftKey },
+  }: {
+    index: number;
+    event: {
+      shiftKey?: boolean;
+      ctrlKey?: boolean;
+    };
+  }) {
+    if (this.lastSelected() === null) {
+      this.handleSingleSelect(index);
+    }
+
+    if (!ctrlKey && !shiftKey) {
+      this.handleSingleSelect(index);
+      return;
+    }
+
+    if (ctrlKey) {
+      this.handleCtrlSelect(index);
+      return;
+    }
+
+    this.handleShiftSelect(index);
+  }
+
+  private handleSingleSelect(index: number) {
+    this.lastSelected.set(index);
+    this._selected.set(new Set([index]));
+  }
+
+  private handleCtrlSelect(index: number) {
+    this.lastSelected.set(index);
+    this._selected.update((items) => produce(items, (draft) => draft.add(index)));
+  }
+
+  private handleShiftSelect(index: number) {
+    const lastSelectedValue = this.lastSelected()!;
+    const [start, end] = this.getSelectionRange(lastSelectedValue, index);
+
+    this._selected.update((items) =>
+      produce(items, (draft) => {
+        draft.clear();
+        for (let index2 = start; index2 <= end; index2++) {
+          draft.add(index2);
+        }
+      }),
+    );
+  }
+
+  private getSelectionRange(start: number, end: number): [number, number] {
+    return [start, end].sort((itemA, itemB) => itemA - itemB) as [number, number];
+  }
+
+  isSelected(index: number) {
+    return this._selected().has(index);
+  }
+
+  clear() {
+    this._selected.set(new Set());
+    this.lastSelected.set(null);
+  }
+}
